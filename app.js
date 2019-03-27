4235b58a8498472c4a33447865293dafed263286
LechCieslak <cieslak.lech@gmail.com>
2019-03-27 18:16:58 +0100

    for loop to random no

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 let randomNumber = () => {
-    let a = Math.ceil(Math.random() * 49);
+    let minNum = 0;     //losowana wartosc jest wieksza od min
+    let maxNum = 49;    //losowana wartosc jest mniejsza lub rowna od max
+
+    let a = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     
-    let b = Math.ceil(Math.random() * 49);
+    let b = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     while (b==a) {
-        b = Math.ceil(Math.random() * 49);
+        b = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     }
-    let c = Math.ceil(Math.random() * 49);
+    let c = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     while (c==a||c==b) {
-        c = Math.ceil(Math.random() * 49);
+        c = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     }
-    let d = Math.ceil(Math.random() * 49);
+    let d = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     while (d==a||d==b||d==c) {
-        d = Math.ceil(Math.random() * 49);
+        d = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     }
-    let e = Math.ceil(Math.random() * 49);
+    let e = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     while (e==a||e==b||e==c||e==d) {
-        e = Math.ceil(Math.random() * 49);
+        e = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     }
-    let f = Math.ceil(Math.random() * 49);
+    let f = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     while (f==a||f==b||f==c||f==d||f==e) {
-        f = Math.ceil(Math.random() * 49);
+        f = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
     }
 
 
@@ -31,3 +34,23 @@ let randomNumber = () => {
     document.getElementsByClassName("no5")[0].innerHTML = e;
     document.getElementsByClassName("no6")[0].innerHTML = f;
 }
+
+let randomNumberWithforLoop = () => {
+    let minNum = 0;     //losowana wartosc jest wieksza od min
+    let maxNum = 6;    //losowana wartosc jest mniejsza lub rowna od max
+    let luckyNumbers = [];
+
+    let a = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
+
+    for(let i=0;i<6;i++) {
+
+        luckyNumbers[i] = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
+        // wyeliminowac powtorzenia, jakims if-em pewnie
+      }
+
+      console.log(luckyNumbers);
+      
+    
+}
+
+randomNumberWithforLoop();
