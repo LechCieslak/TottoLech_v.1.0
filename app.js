b97b487c3de59b906cb6541d55fec7793cd4981d
LechCieslak <cieslak.lech@gmail.com>
2019-04-02 16:55:29 +0200

    center content

--- app.js
+++ app.js
@@ -2,43 +2,6 @@ function compareNr(a,b) {     //sortowanie
     return a-b;
 }
 
-/*let randomNumberOldVersion = () => {
-    let minNum = 0;     //losowana wartosc jest wieksza od min
-    let maxNum = 49;    //losowana wartosc jest mniejsza lub rowna od max
-
-    let a = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    
-    let b = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    while (b==a) {
-        b = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    }
-    let c = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    while (c==a||c==b) {
-        c = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    }
-    let d = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    while (d==a||d==b||d==c) {
-        d = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    }
-    let e = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    while (e==a||e==b||e==c||e==d) {
-        e = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    }
-    let f = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    while (f==a||f==b||f==c||f==d||f==e) {
-        f = Math.ceil(Math.random() * (maxNum - minNum)) + minNum;
-    }
-
-
-        
-    document.getElementsByClassName("no1")[0].innerHTML = a;
-    document.getElementsByClassName("no2")[0].innerHTML = b;
-    document.getElementsByClassName("no3")[0].innerHTML = c;
-    document.getElementsByClassName("no4")[0].innerHTML = d;
-    document.getElementsByClassName("no5")[0].innerHTML = e;
-    document.getElementsByClassName("no6")[0].innerHTML = f;
-}*/
-
 let randomNumber = () => {
     let minNum = 0;     //losowana wartosc jest wieksza od min
     let maxNum = 49;    //losowana wartosc jest mniejsza lub rowna od max
@@ -57,6 +20,7 @@ let randomNumber = () => {
       }
 
       document.getElementsByClassName("no1")[0].innerHTML = luckyNumbers[0];
+      // document.querySelectorAll(".no1")[0].innerHTML = luckyNumbers[0]; //tak też może być
       document.getElementsByClassName("no2")[0].innerHTML = luckyNumbers[1];
       document.getElementsByClassName("no3")[0].innerHTML = luckyNumbers[2];
       document.getElementsByClassName("no4")[0].innerHTML = luckyNumbers[3];
